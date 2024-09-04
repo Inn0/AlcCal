7ae09cb74c4abf17834ac92296f719a314b172cd
Daan <daan.brocatus@outlook.com>
2024-09-04 12:32:03 +0200

    feat: small improvements to swipe to delete and header

--- app/src/main/java/nl/daanbrocatus/alccal/composables/common/SwipeToDeleteContainer.kt
+++ app/src/main/java/nl/daanbrocatus/alccal/composables/common/SwipeToDeleteContainer.kt
@@ -1,6 +1,5 @@
 package nl.daanbrocatus.alccal.composables.common
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SwipeToDismiss
@@ -24,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -42,8 +43,7 @@ fun <T> SwipeToDeleteContainer(
     val state = rememberSwipeToDismissBoxState()
 
     LaunchedEffect(state.progress) {
-        Log.d("SwipeToDeleteContainer", "Progress: ${state.progress}")
-        if (state.progress >= 0.7f && state.progress < 1.0f) {
+        if (state.targetValue == SwipeToDismissBoxValue.EndToStart && state.progress > 0.5f) {
             isRemoved = true
         }
     }
@@ -84,7 +84,8 @@ fun DeleteBackground(
 
     Box(
         modifier = Modifier
-            .padding(top = 4.dp, bottom = 4.dp, start = 8.dp, end = 0.dp)
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .clip(RoundedCornerShape(12.dp))
             .fillMaxSize()
             .background(color),
         contentAlignment = Alignment.CenterEnd
